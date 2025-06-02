1cbf0f46644c4152259ec0acd8bdd880150e8754
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-02 13:32:09 +0200

    changement des nom de certains exercices pour les mettre dans le bonne odre et ajouts de nouveau exercice

---
+++ Exercice 11 Note/Program.cs
@@ -0,0 +1,41 @@
+﻿//on définie une variable nombre et on l'initialise avec 0
+double nombre = 0;
+//on définie une variable success et on l'initialise avec faux
+bool success = false;
+
+//on boucle tant que success est faux
+while (!success)
+{
+    //on demande à l'utilisateur d'entrée un nombre entier
+    Console.Write("Rentrez un nombre réel : ");
+    //on déclare une variable et on y assigne la valeur entrée par l'utilisateur
+    string asked = Console.ReadLine();
+
+    //on s'assure que l'utilisateur n'as pas entrée une valeur null dans asked
+    if (asked == null)
+    {
+        //on recomance la boucle du début quand asked  est null
+        continue;
+    }
+
+    //on essaye de convertir ce que l'utilisateur à taper en un nombre
+    //success = true si la convertions est réussi et false si ça à échoué
+    success = double.TryParse(asked, out nombre);
+}
+
+//on regarde si le nombre est supérieur ou égale à 10
+if (nombre >= 10)
+{
+    Console.WriteLine("Admis");
+
+}
+//on regarde si le nombre est supérieur à 8
+else if (nombre > 8)
+{
+    Console.WriteLine("Rattrapage");
+}
+//ici le nombre est inferieur à 8
+else
+{
+    Console.WriteLine("Echec");
+}
---
+++ Exercice 14 Consigne/Program.cs
@@ -0,0 +1,24 @@
+﻿//on affiche une demande à l'utilisateur
+Console.Write("Entrez un charactère : ");
+//on récupère ce que tape l'utilisateur et le met dans la variable charactère
+char charactère = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+//on fait un switch case sur le charactère
+switch (charactère)
+{
+    //dans le cas ou le charactère est o ou O
+    case 'o':
+    case 'O':
+        Console.WriteLine("Affirmatif");
+        break;
+    //dans le cas ou le charactère est n ou N
+    case 'n':
+    case 'N':
+        Console.WriteLine("Négatif");
+        break;
+    //dans les autres cas
+    default:
+        Console.WriteLine("???");
+        break;
+}
