e3e3cf580b33fb53590290bdf50494be0782481d
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-03 10:32:06 +0200

    Amélioration de la gestion des opérations mathématiques
    Refactorisation du fichier `Program.cs` pour remplacer la variable temporaire `temp` par `currentNumber`, clarifiant ainsi son rôle. Ajout de la méthode `addToList` pour simplifier l'ajout d'opérateurs et de nombres à la liste des opérations. Des commentaires ont été mis à jour pour mieux expliquer le code, et des vérifications supplémentaires ont été intégrées pour valider les entrées utilisateur, notamment pour éviter les opérateurs consécutifs. Suppression de lignes de code inutiles pour améliorer la lisibilité et l'efficacité.

--- Exercice bonus/Program.cs
+++ Exercice bonus/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace Exercice_bonus
@@ -33,74 +31,61 @@ namespace Exercice_bonus
             //on affecte à asked sa valeur sans les charactère invisible
             asked = sWhitespace.Replace(asked, "");
 
-            //on déclare et assigne une variable temporaire appeler temp
-            string temp = "";
+            //on déclare et assigne une variable temporaire appeler currentNumber
+            string currentNumber = "";
 
             //on boucle sur tous les charactère de la chaine contenue dans variable
             for (int i = 0; i < asked.Length;  i++)
             {
-                //on regarde si le charactère est un opérateur ou un charactère invisible
-                if (IsAnOperator(asked[i]) || IsAPranthesis(asked[i]))
+                //on regarde si le charactère n'est pas un opérateur ou un charactère invisible
+                if (!IsAnOperator(asked[i]) && !IsAPranthesis(asked[i]))
                 {
-                    //si temp n'est pas vide et si temp n'est pas un nombre entier
-                    if (temp != "" && !IsInteger(temp))
-                    {
-                        // on ferme le programme
-                        return;
-                    }
-
-                    //on ajoute le contenue de temp dans operation
-                    operations.AddLast(temp);
-
-                    //on regarde si i est supérieur à 0
-                    if (i > 0)
-                    {
-                        //on regarde si l'utilisateur n'as pas taper deux fois un opérateur
-                        if (IsAnOperator(asked[i - 1]) && IsAnOperator(asked[i]))
-                        {
-                            Console.WriteLine("Il ne peux pas y avoir deux fois un opérateur à la suite des autres");
-                            //on ferme le programme
-                            return;
-                        }
-
-                        //on regarde si le charactère précédent est une parenthèse et que l'opérateur est une multiplication ou une division
-                        if (asked[i - 1] == '(' && IsDiviseOrMultiply(asked[i]))
-                        {
-                            Console.WriteLine("L'opérateur après une parenthèse ne peux être une multiplisation ou division");
-                            //on ferme le programme
-                            return;
-                        }
-                    }
-
-                    //on assigne à temp une valeur vide
-                    temp = "";
-
-                    if (asked[i] == '-')
-                    {
-                        temp += "-";
-                    } else
-                    {
-                        //on ajoute l'opérateur / la parenthèse à la liste opérations
-                        operations.AddLast(asked[i].ToString());
-                    }
-
-                    //on continue la boucle sans finir les instruction
+                    //on mets dans asked le charactère qui n'est pas une opération ou une parenthèse
+                    currentNumber += asked[i];
+                    //on continue la boucle sans faire les instructions suivantes
                     continue;
                 }
 
-                //on mets dans asked le charactère qui n'est pas une opération ou une parenthèse
-                temp += asked[i];
+                //si currentNumber n'est pas vide et si currentNumber n'est pas un nombre entier
+                if (currentNumber != "" && !IsInteger(currentNumber))
+                {
+                    // on ferme le programme
+                    return;
+                }
+
+                try
+                {
+                    //on regarde si l'utilisateur n'as pas taper deux fois un opérateur
+                    if (IsAnOperator(asked[i - 1]) && IsAnOperator(asked[i]))
+                    {
+                        Console.WriteLine("Il ne peux pas y avoir deux fois un opérateur à la suite des autres");
+                        //on ferme le programme
+                        return;
+                    }
+
+                    //on regarde si le charactère précédent est une parenthèse et que l'opérateur est une multiplication ou une division
+                    if (asked[i - 1] == '(' && IsDiviseOrMultiply(asked[i]))
+                    {
+                        Console.WriteLine("L'opérateur après une parenthèse ne peux être une multiplisation ou division");
+                        //on ferme le programme
+                        return;
+                    }
+                }
+                catch (IndexOutOfRangeException) { }
+
+                //on ajoute à la liste l'opérateur et le chiffre actuelle
+                currentNumber = addToList(operations, currentNumber, asked[i]);
             }
 
-            //si temp n'est pas vide on mets dans opération sa valeur
-            if(temp != "")
+            //si currentNumber n'est pas vide on mets dans opération sa valeur
+            if(currentNumber != "")
             {
-                if(!IsInteger(temp))
+                if(!IsInteger(currentNumber))
                 {
                     //on ferme le program
                     return;
                 }
-                operations.AddLast(temp);
+                operations.AddLast(currentNumber);
             }
 
             Console.WriteLine(JsonSerializer.Serialize(operations));
@@ -112,10 +97,6 @@ namespace Exercice_bonus
             return false;
         }
 
-        /**
-         * <param  name="value">La chaine de charactère à verifier</param>
-         * <returns>Si la chaine est un entier ou non</returns>
-         **/
         public static bool IsInteger(string value)
         {
             //on essaye de convertir la chaine de charactère en un nombre entier et on stock dans une varible si ça à marcher
@@ -144,6 +125,28 @@ namespace Exercice_bonus
         public static bool IsDiviseOrMultiply(char character)
         {
             return character == '*' || character == '/';
+        }
+
+        public static string addToList(LinkedList<string> operations, string temp, char operation)
+        {
+            //on regarde si temp n'est pas vide
+            if(temp != "")
+            {
+                //on ajoute le contenue de currentNumber dans operation
+                operations.AddLast(temp);
+            }
+
+            //on regarde si le charactère est une soustraction
+            if (operation == '-')
+            {
+                //on assigne le signe - à currentNumber
+                return "-";
+            }
+
+            //on ajoute l'opérateur ou la parenthèse à la liste opérations
+            operations.AddLast(operation.ToString());
+            //on assigne une valeur vide à currentNumber
+            return "";
         }
     }
 }
