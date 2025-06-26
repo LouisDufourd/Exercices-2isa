b69dc1106c05923ea36d2d840545b9c941c66297
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-26 10:10:46 +0200

    Amélioration de la fonction Compress et refactorisation des tableaux
    La fonction `Compress` a été enrichie de commentaires explicatifs en français pour clarifier son fonctionnement. La logique de compression a été ajustée pour une meilleure lisibilité.
    
    De plus, le type de `resultArray` a été modifié d'une liste dynamique (`List<int>`) à un tableau statique (`int[]`), nécessitant des ajustements dans les opérations de multiplication et d'addition. Des commentaires ont également été ajoutés pour expliquer les étapes du calcul.

--- Exercice 045 Compression/Program.cs
+++ Exercice 045 Compression/Program.cs
@@ -1,27 +1,45 @@
-﻿string Compress(string? input)
+﻿//la fonction prend en paramètre une chaine de charactère input et retourne la chaine comprésser
+string Compress(string? input)
 {
+    //si la chaine de charactère est null on retourne une chaine vide
     if(input == null) return "";
 
+    //on crée une chaine de charactère comprésser qui serviras à contenir l'input comprésser
     string compressed = "";
+    //on assigne le charactère précédent au charactère NULL
     char currentChar = (char)0;
+    //on initialise une quantité à 1
     int quantity = 1;
 
-    foreach (var character in input)
+    //on parcours la chaine de charactère et on crée une variable qui stockera le charactère séléctionné
+    foreach (char character in input)
     {
+        //on regarde si le précédent charactère est différent du charactère actuelle si il est différent on ajoute le précédent charactère avec sa quantité à compressed
         if (currentChar != character)
         {
+            //on ajoute à compressed la quantity si elle est supérieur à 1 sinon on met ajoute une chaine vide puis on ajoute le précédent charactère
             compressed += (quantity > 1 ? $"{quantity}" : "") + currentChar;
+            //on assigne le précédent character avec le caractère actuelle
             currentChar = character;
+            //on met la quantité à 1
             quantity = 1;
         }
+        //on incrémente la quantité
         else quantity++;
     }
 
+    // ? : est un if else avec une retour ex : condition ? si true : si false
     compressed += (quantity > 1 ? $"{quantity}" : "") + currentChar;
 
+    //on retourne compresser
     return compressed;
 }
 
+//on affiche à l'utilisateur une demande d'entrée de chaine de charactère
 Console.Write("Rentrez un chaine de charactère à compresser : ");
+
+//on écrit dans text ce que l'utilisateur à taper
 string? text = Console.ReadLine();
+
+//on affiche la chane de charactère compresser
 Console.WriteLine($"Version compressez: {Compress(text)}");
--- Exercice 052 Schtroumpf/Program.cs
+++ Exercice 052 Schtroumpf/Program.cs
@@ -1,34 +1,38 @@
 ﻿int[] firstArray = [4,8,7,12];
 int[] secondArray = [3,6];
-List<int> resultArray = new();
+int[] resultArray = new int[firstArray.Length *  secondArray.Length];
 
 string operation = "";
+
+int k = 0;
 
 for (int i = 0; i < secondArray.Length; i++)
 {
     int firstNumber = secondArray[i];
+    
     for (int j = 0; j < firstArray.Length; j++)
     {
-        var secondNumber = firstArray[j];
+        int secondNumber = firstArray[j];
         int factor = firstNumber * secondNumber;
-        resultArray.Add(factor);
-
+        resultArray[k] = factor;
         operation += $"{firstNumber} * {secondNumber} = {factor}\n";
+        k++;
     }
 }
 
-while(resultArray.Count > 1)
+while(resultArray.Length > 1)
 {
-    List<int> temp = [];
-    for (int i = 0; i < resultArray.Count - 1; i += 2)
+    int[] temp = new int[resultArray.Length / 2];
+    for (int i = 0; i < resultArray.Length - 1; i += 2)
     {
         int result = resultArray[i] + resultArray[i + 1];
 
-        temp.Add(result);
+        temp[i / 2] = result;
 
         operation += $"{resultArray[i]} + {resultArray[i + 1]} = {result}\n";
     }
 
+    resultArray = new int[temp.Length];
     resultArray = temp;
 } 
 
